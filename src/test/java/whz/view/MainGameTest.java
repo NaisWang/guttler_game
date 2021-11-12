336ad6b9393279ff0f89ded3524c3f3535945008
DreamFields <1339611623@qq.com>
2021-11-12 19:51:31 +0800

    'GUI测试跑通'

--- src/test/java/whz/view/MainGameTest.java
+++ src/test/java/whz/view/MainGameTest.java
@@ -3,6 +3,7 @@ import whz.test.SnakeGameTest;
 
 import org.junit.After;
 import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JButtonFixture;
 import org.junit.Before;
 import org.junit.Test;
 import whz.util.Global;
@@ -11,30 +12,36 @@ public class MainGameTest {
 
     private FrameFixture frameTest;
 
+    private SnakeGameTest mySnakeGame = new SnakeGameTest();
+
     @Before
     public void setUp() {
-        frameTest = new FrameFixture(new SnakeGameTest());
-        frameTest.show();
+        frameTest = new FrameFixture(mySnakeGame);
+//        frameTest.show(); // 不要加这行代码，会使得整个frame变回原始大小（没有长宽）
+    }
+
+    @Test
+    public void testSingleMode() {
+//        监听单人模式按钮
+        JButtonFixture singleBtn=frameTest.button("singleBtn");
+        singleBtn.click();
+        singleBtn.requireNotVisible();
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    @Test
-    public void testSingleMode() {
-        System.out.println(frameTest.button("singleBtn"));
-        frameTest.button("singleBtn").click();
-    }
+
 
     @Test
     public void testDoubleMode() {
         frameTest.button("doubleBtn").click();
     }
 
-//    @After
-//    public void tearDown() {
-//        frameTest.cleanUp();
-//    }
+    @After
+    public void tearDown() {
+        frameTest.cleanUp();
+    }
 }
